1c4e534e6995bc0674c84b87a99fd91c903ad2d6
VioletGiraffe <VioletGiraffe@users.noreply.github.com>
2016-04-01 22:12:09 +0300

    AdvancedAssert configured to print failed assertions to qDebug

--- app/main.cpp
+++ app/main.cpp
@@ -1,12 +1,18 @@
 #include "cmainwindow.h"
 #include "settings/csettings.h"
+#include "assert/advanced_assert.h"
 
 DISABLE_COMPILER_WARNINGS
 #include <QApplication>
+#include <QDebug>
 RESTORE_COMPILER_WARNINGS
 
 int main(int argc, char *argv[])
 {
+	AdvancedAssert::setLoggingFunc([](const char* message){
+		qDebug() << message;
+	});
+
 	QApplication app(argc, argv);
 	app.setQuitOnLastWindowClosed(false);
 
